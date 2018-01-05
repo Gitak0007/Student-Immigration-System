4e2cdbed8d4a60d7fc6666f8de8bd3d0acf259c3
gitak0007 <sp17mcsw0007@maju.com.pk>
2018-01-06 02:32:23 +0500

    Profile and universities work done

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,9 @@ router.get("/login",function(req,res,next){
 	res.render("login",{title:"Login To Your Account"});
 })
 
-router.get('/addProfile', function(req, res, next) {
+router.get('/Profile', function(req, res, next) {
   console.log("post request fetch", req.body); 
-  res.render('addProfile', { title: 'Add Profile',data:req.body  });
+  res.render('Profile', { title: 'Add Profile',data:req.body  });
 });
 
 router.get('/ListofUniversities', function(req, res, next) {
@@ -44,7 +44,7 @@ router.post("/signup",function(req,res,next){
 		}
 	});
 	if(newuser){
-		res.redirect("/addProfile");
+		res.redirect("/login");
 	}
 	
 })
@@ -59,7 +59,7 @@ router.post("/login",function(req,res,next){
 		if(results.length>0)
 		{
 			
-			res.redirect("/addProfile");
+			res.redirect("/Profile");
 		}
 		if(results.length==0){
 			error="User not exists";
@@ -68,5 +68,56 @@ router.post("/login",function(req,res,next){
 	})
 
 })
+
+router.post("/Profile",function(req,res,next){
+	console.log(req.body);
+	var Gre=req.body.Gre;
+	var LanguageScore=req.body.LanguageScore;
+	var GPA=req.body.GPA;
+	var JobExperience=req.body.JobExperience;
+	var Internship=req.body.Internship;
+	var Publications=req.body.Publications;
+
+	var profile={
+		"GREScore":Gre,
+		"languageScore":LanguageScore,
+		"bachelorCGPA":GPA,
+		"JobExp":JobExperience,
+		"Internship":Internship,
+		"Publication":Publications
+	}
+	var profileAdd=db.query('insert into profile SET ?',profile,function (error, results, fields){
+		if (error) {
+			console.log(error);
+		}
+		
+		if(fields){
+			console.log(fields);
+		}
+	});
+	if(profileAdd){
+		if(GPA>3){
+			var uniA=db.query("Select * from university where uniGrade='C'",function(error,results,fields){
+				if(results.length>0)
+				{
+					console.log(results);	
+				res.render("ListofUniversities",{title:"Universites Matching Your Profile ",results:results});
+				}				
+			})
+		}
+		else {
+			var uniA=db.query("Select * from university where uniGrade='B'",function(error,results,fields){
+				if(results.length>0)
+				{
+					console.log(results);	
+				res.render("ListofUniversities",{title:"Universites Matching Your Profile ",results:results});
+				}				
+			})
+		}
+		}
+	}
+	
+)
+
 module.exports = router;
 
